14e69fdb5ea27ed7587c0448b8c7b7a650465e84
kwangyong kim <yongyong@bpsoft.co.kr>
2023-06-01 16:46:30 +0900

    - 컨트롤러 추가.

---
+++ sc/src/main/java/com/yongyong/sc/controller/MainContorller.java
@@ -0,0 +1,15 @@
+package com.yongyong.sc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainContorller {
+
+    @RequestMapping(value = "/")
+    public String main(){
+
+        return "index.html";
+    }
+
+}
